1816bb1635e1a772745cc51c7d51ef14a6df85d5
Kael Greco <kael.greco@gmail.com>
2020-02-29 14:18:50 -0800

    farmhash

--- web-bundle/src/main/java/com/graphhopper/swl/CustomCarFlagEncoder.java
+++ web-bundle/src/main/java/com/graphhopper/swl/CustomCarFlagEncoder.java
@@ -22,14 +22,14 @@ import com.graphhopper.routing.profiles.EncodedValue;
 import com.graphhopper.routing.profiles.UnsignedIntEncodedValue;
 import com.graphhopper.routing.util.CarFlagEncoder;
 import com.graphhopper.util.EdgeIteratorState;
+import com.google.common.primitives.Longs;
 
-import javax.xml.bind.DatatypeConverter;
 import java.util.List;
 
 public class CustomCarFlagEncoder extends CarFlagEncoder {
 
-    private UnsignedIntEncodedValue[] stableIdByte = new UnsignedIntEncodedValue[16];
-    private UnsignedIntEncodedValue[] reverseStableIdByte = new UnsignedIntEncodedValue[16];
+    private UnsignedIntEncodedValue[] stableIdEnc = new UnsignedIntEncodedValue[8];
+    private UnsignedIntEncodedValue[] reverseStableIdEnc = new UnsignedIntEncodedValue[8];
 
     public CustomCarFlagEncoder() {
         super();
@@ -39,34 +39,35 @@ public class CustomCarFlagEncoder extends CarFlagEncoder {
     @Override
     public void createEncodedValues(List<EncodedValue> registerNewEncodedValue, String prefix, int index) {
         super.createEncodedValues(registerNewEncodedValue, prefix, index);
-        for (int i=0; i<16; i++) {
-            stableIdByte[i] = new UnsignedIntEncodedValue("stable-id-byte-"+i, 8, false);
-            registerNewEncodedValue.add(stableIdByte[i]);
+        for (int i=0; i<8; i++) {
+            stableIdEnc[i] = new UnsignedIntEncodedValue("stable-id-byte-"+i, 8, false);
+            registerNewEncodedValue.add(stableIdEnc[i]);
         }
-        for (int i=0; i<16; i++) {
-            reverseStableIdByte[i] = new UnsignedIntEncodedValue("reverse-stable-id-byte-"+i, 8, false);
-            registerNewEncodedValue.add(reverseStableIdByte[i]);
+        for (int i=0; i<8; i++) {
+            reverseStableIdEnc[i] = new UnsignedIntEncodedValue("reverse-stable-id-byte-"+i, 8, false);
+            registerNewEncodedValue.add(reverseStableIdEnc[i]);
         }
     }
 
     final String getStableId(boolean reverse, EdgeIteratorState edge) {
-        byte[] stableId = new byte[16];
-        UnsignedIntEncodedValue[] idByte = reverse ? reverseStableIdByte : stableIdByte;
-        for (int i=0; i<16; i++) {
+        byte[] stableId = new byte[8];
+        UnsignedIntEncodedValue[] idByte = reverse ? reverseStableIdEnc : stableIdEnc;
+        for (int i=0; i<8; i++) {
             stableId[i] = (byte) edge.get(idByte[i]);
         }
-        return DatatypeConverter.printHexBinary(stableId).toLowerCase();
+        return Long.toUnsignedString(Longs.fromByteArray(stableId));
     }
 
     final void setStableId(boolean reverse, EdgeIteratorState edge, String stableId) {
-        byte[] stableIdBytes = DatatypeConverter.parseHexBinary(stableId);
+        long stableIdLong = Long.parseUnsignedLong(stableId);
+        byte[] stableIdBytes = Longs.toByteArray(stableIdLong);
 
-        if (stableIdBytes.length != 16)
-            throw new IllegalArgumentException("stable ID must be 16 bytes: " + DatatypeConverter.printHexBinary(stableIdBytes));
+        if (stableIdBytes.length != 8)
+            throw new IllegalArgumentException("stable ID must be 8 bytes: " + stableIdBytes);
 
-        UnsignedIntEncodedValue[] idByte = reverse ? reverseStableIdByte : stableIdByte;
-        for (int i=0; i<16; i++) {
-            edge.set(idByte[i], Byte.toUnsignedInt(stableIdBytes[i]));
+        UnsignedIntEncodedValue[] idBytes = reverse ? reverseStableIdEnc : stableIdEnc;
+        for (int i=0; i<8; i++) {
+            edge.set(idBytes[i], Byte.toUnsignedInt(stableIdBytes[i]));
         }
     }
 
