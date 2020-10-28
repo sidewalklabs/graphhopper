aede3680c64ad128e78f7e71c8a57abe972b641f
Mark Idleman <mark@replicahq.com>
2020-10-27 18:35:36 -0700

    add per-edge distances to street path responses

--- grpc/src/main/java/RouterImpl.java
+++ grpc/src/main/java/RouterImpl.java
@@ -80,7 +80,7 @@ public class RouterImpl extends router.RouterGrpc.RouterImplBase {
         );
         ghRequest.setProfile(request.getProfile());
         ghRequest.setLocale(Locale.US);
-        ghRequest.setPathDetails(Lists.newArrayList("stable_edge_ids"));
+        ghRequest.setPathDetails(Lists.newArrayList("stable_edge_ids", "distance"));
 
         PMap hints = new PMap();
         hints.putObject(INSTRUCTIONS, false);
@@ -99,11 +99,15 @@ public class RouterImpl extends router.RouterGrpc.RouterImplBase {
             List<String> pathStableEdgeIds = responsePath.getPathDetails().get("stable_edge_ids").stream()
                     .map(pathDetail -> (String) pathDetail.getValue())
                     .collect(Collectors.toList());
+            List<Double> edgeDistances = responsePath.getPathDetails().get("distance").stream()
+                    .map(pathDetail -> (Double) pathDetail.getValue())
+                    .collect(Collectors.toList());
 
             replyBuilder.addPaths(StreetPath.newBuilder()
                     .setTime(responsePath.getTime())
                     .setDistance(responsePath.getDistance())
                     .addAllStableEdgeIds(pathStableEdgeIds)
+                    .addAllDistances(edgeDistances)
                     .setPoints(WebHelper.encodePolyline(responsePath.getPoints()))
             );
         }
