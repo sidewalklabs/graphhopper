0b6635a478a55b0e201eca02c2f76251a4fc87b7
Mark Idleman <mark@replicahq.com>
2020-12-03 11:45:24 -0800

    try fixing build again

--- build_with_proto.sh
+++ build_with_proto.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p ./grpc/src/main/proto
 cp ./idls/model/router.proto ./grpc/src/main/proto/router.proto
-mvn -s maven_settings.xml --projects grpc web -am -DskipTests=true package
+mvn -s maven_settings.xml --projects grpc -am -DskipTests=true package
+mvn -s maven_settings.xml --projects web -am -DskipTests=true package
