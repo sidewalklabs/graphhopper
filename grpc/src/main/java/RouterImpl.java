57580db4bc1fc496b9873cbce1fb84d24fe6ed66
Mark Idleman <mark@replicahq.com>
2021-01-06 17:54:33 -0800

    Hotfix: add GTFS feed ID to stop names in GRPC transit impl (mega_router_v2.1)

--- grpc/src/main/java/RouterImpl.java
+++ grpc/src/main/java/RouterImpl.java
@@ -52,17 +52,20 @@ public class RouterImpl extends router.RouterGrpc.RouterImplBase {
     private final MatrixAPI matrixAPI;
     private Map<String, String> gtfsLinkMappings;
     private Map<String, String> gtfsRouteInfo;
+    private Map<String, String> gtfsFeedIdMapping;
     // private final StatsDClient statsDClient;
 
     public RouterImpl(GraphHopper graphHopper, PtRouter ptRouter, MatrixAPI matrixAPI,
                       Map<String, String> gtfsLinkMappings,
-                      Map<String, String> gtfsRouteInfo
+                      Map<String, String> gtfsRouteInfo,
+                      Map<String, String> gtfsFeedIdMapping
                       /*StatsDClient statsDClient*/) {
         this.graphHopper = graphHopper;
         this.ptRouter = ptRouter;
         this.matrixAPI = matrixAPI;
         this.gtfsLinkMappings = gtfsLinkMappings;
         this.gtfsRouteInfo = gtfsRouteInfo;
+        this.gtfsFeedIdMapping = gtfsFeedIdMapping;
         // this.statsDClient = statsDClient;
     }
 
@@ -340,10 +343,10 @@ public class RouterImpl extends router.RouterGrpc.RouterImplBase {
         public final String routeLongName;
         public final String routeType;
 
-        public CustomPtLeg(Trip.PtLeg leg, List<String> stableEdgeIds, String agencyName, String routeShortName,
-                           String routeLongName, String routeType) {
+        public CustomPtLeg(Trip.PtLeg leg, List<String> stableEdgeIds, List<Trip.Stop> updatedStops,
+                           String agencyName, String routeShortName, String routeLongName, String routeType) {
             super(leg.feed_id, leg.isInSameVehicleAsPrevious, leg.trip_id, leg.route_id,
-                    leg.trip_headsign, leg.stops, leg.distance, leg.travelTime, leg.geometry);
+                    leg.trip_headsign, updatedStops, leg.distance, leg.travelTime, leg.geometry);
             this.stableEdgeIds = stableEdgeIds;
             this.agencyName = agencyName;
             this.routeShortName = routeShortName;
@@ -385,6 +388,15 @@ public class RouterImpl extends router.RouterGrpc.RouterImplBase {
             logger.info("Failed to find route info for route " + leg.route_id + " for PT trip leg " + leg.toString());
         }
 
-        return new CustomPtLeg(leg, stableEdgeIdsList, routeInfo[0], routeInfo[1], routeInfo[2], routeInfo[3]);
+        // Add proper GTFS feed ID as prefix to all stop names in Leg
+        List<Trip.Stop> updatedStops = Lists.newArrayList();
+        for (Trip.Stop stop : leg.stops) {
+            String updatedStopId = gtfsFeedIdMapping.get(leg.feed_id) + ":" + stop.stop_id;
+            updatedStops.add(new Trip.Stop(updatedStopId, stop.stop_name, stop.geometry, stop.arrivalTime,
+                    stop.plannedArrivalTime, stop.predictedArrivalTime, stop.arrivalCancelled, stop.departureTime,
+                    stop.plannedDepartureTime, stop.predictedDepartureTime, stop.departureCancelled));
+        }
+
+        return new CustomPtLeg(leg, stableEdgeIdsList, updatedStops, routeInfo[0], routeInfo[1], routeInfo[2], routeInfo[3]);
     }
 }
