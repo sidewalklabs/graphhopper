928bb4d0321a5ac1ca378b41f33e2fe37d176e30
Kael Greco <kael.greco@gmail.com>
2021-03-16 15:52:07 -0700

    tripIdInRoute

--- web-bundle/src/main/java/com/graphhopper/replica/GtfsLinkMapper.java
+++ web-bundle/src/main/java/com/graphhopper/replica/GtfsLinkMapper.java
@@ -224,10 +224,11 @@ public class GtfsLinkMapper {
                                                    HTreeMap<String, String> gtfsLinkMappings) {
         List<String> rowsForFeed = Lists.newArrayList();
         for (String routeId : routeIdToTripsInRoute.keySet()) {
-	    if (!routeIdToTripsInRoute.containsKey(routeId)) {
-		    continue;
-	    }
 	    for (String tripIdInRoute : routeIdToTripsInRoute.get(routeId)) {
+	        if (!tripIdToStopPairsInTrip.containsKey(tripIdInRoute)) {
+		    continue;
+		}
+
                 for (Pair<Stop, Stop> stopStopPair : tripIdToStopPairsInTrip.get(tripIdInRoute)) {
                     // Filter out stop-stop pairs where the stops are identical
                     if (stopStopPair.getLeft().stop_id.equals(stopStopPair.getRight().stop_id)) {
