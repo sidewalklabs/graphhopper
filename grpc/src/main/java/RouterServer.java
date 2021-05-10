e1ba143c967d046cbceb4131eae24eea6a61ab5a
Mark Idleman <mark@replicahq.com>
2021-05-10 16:11:27 -0700

    make GH read-only; add env-var settable server settings

--- grpc/src/main/java/RouterServer.java
+++ grpc/src/main/java/RouterServer.java
@@ -19,6 +19,8 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.GraphHopperConfig;
 import com.graphhopper.gtfs.GraphHopperGtfs;
@@ -39,9 +41,10 @@ import io.dropwizard.bundles.redirect.RedirectBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.grpc.Server;
+import io.grpc.internal.GrpcUtil;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
-import io.grpc.netty.shaded.io.netty.channel.epoll.EpollEventLoopGroup;
-import io.grpc.netty.shaded.io.netty.channel.epoll.EpollServerSocketChannel;
+import io.grpc.netty.shaded.io.netty.channel.nio.NioEventLoopGroup;
+import io.grpc.netty.shaded.io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.grpc.protobuf.services.ProtoReflectionService;
 import io.grpcweb.GrpcPortNumRelay;
 import io.grpcweb.GrpcWebTrafficServlet;
@@ -53,6 +56,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -61,16 +65,25 @@ public class RouterServer {
     private static final Logger logger = LoggerFactory.getLogger(RouterServer.class);
     private Server server;
     private String configPath;
-    private int numThreads;
-    private int maxConnTime;
-    private int maxConcCalls;
+    private Map<String, Integer> defaultProperties;
+    private Map<String, Integer> userDefinedProperties;
     private GraphHopperManaged graphHopperManaged;
+    public static final Set<String> SETTABLE_PARAMETERS = Sets.newHashSet(
+            "SERVER_THREADS",
+            "BOSS_EVENT_LOOP_THREADS",
+            "WORKER_EVENT_LOOP_THREADS",
+            "CONN_TIME_MAX_AGE_SECS",
+            "CONN_TIME_GRACE_PERIOD_SECS",
+            "MAX_CONC_CALLS_PER_CONN",
+            "KEEP_ALIVE_TIME_SECS",
+            "KEEP_ALIVE_TIMEOUT_SECS",
+            "FLOW_CONTROL_WINDOW_BYTES"
+    );
 
-    public RouterServer(String configPath, int numThreads, int maxConnTime, int maxConcCalls) {
+    public RouterServer(String configPath, Map<String, Integer> defaultProperties, Map<String, Integer> userDefinedProperties) {
         this.configPath = configPath;
-        this.numThreads = numThreads;
-        this.maxConnTime = maxConnTime;
-        this.maxConcCalls = maxConcCalls;
+        this.defaultProperties = defaultProperties;
+        this.userDefinedProperties = userDefinedProperties;
     }
 
     private void start() throws Exception {
@@ -109,11 +122,15 @@ public class RouterServer {
             logger.info("No GTFS link mapping mapdb file found! Skipped loading GTFS link mappings.");
         }
 
-        // Initialize Datadog client
-        StatsDClient statsDClient = new NonBlockingStatsDClientBuilder()
-                .hostname(System.getenv("DD_AGENT_HOST"))
-                .port(8125)
-                .build();
+        String datadogHost = System.getenv("DD_AGENT_HOST");
+        StatsDClient statsDClient = null;
+        if (datadogHost != null) {
+            // Initialize Datadog client
+            statsDClient = new NonBlockingStatsDClientBuilder()
+                    .hostname(System.getenv("DD_AGENT_HOST"))
+                    .port(8125)
+                    .build();
+        }
 
         logger.info("Datadog agent host IP is: " + System.getenv("DD_AGENT_HOST"));
 
@@ -122,18 +139,21 @@ public class RouterServer {
         server = NettyServerBuilder.forPort(grpcPort)
                 .addService(new RouterImpl(graphHopper, ptRouter, matrixAPI, gtfsLinkMappings, gtfsRouteInfo, gtfsFeedIdMapping, statsDClient))
                 .addService(ProtoReflectionService.newInstance())
-                .maxConnectionAge(maxConnTime, TimeUnit.SECONDS)
-                .maxConnectionAgeGrace(30, TimeUnit.SECONDS)
-                .maxConcurrentCallsPerConnection(maxConcCalls)
-                .executor(Executors.newFixedThreadPool(numThreads))
-                .workerEventLoopGroup(new EpollEventLoopGroup(1))
-                .bossEventLoopGroup(new EpollEventLoopGroup(1))
-                .channelType(EpollServerSocketChannel.class)
+                .maxConnectionAge(userDefinedProperties.getOrDefault("CONN_TIME_MAX_AGE_SECS", defaultProperties.get("CONN_TIME_MAX_AGE_SECS")), TimeUnit.SECONDS)
+                .maxConnectionAgeGrace(userDefinedProperties.getOrDefault("CONN_TIME_GRACE_PERIOD_SECS", defaultProperties.get("CONN_TIME_GRACE_PERIOD_SECS")), TimeUnit.SECONDS)
+                .maxConcurrentCallsPerConnection(userDefinedProperties.getOrDefault("MAX_CONC_CALLS_PER_CONN", defaultProperties.get("MAX_CONC_CALLS_PER_CONN")))
+                .executor(Executors.newFixedThreadPool(userDefinedProperties.getOrDefault("SERVER_THREADS", defaultProperties.get("SERVER_THREADS"))))
+                .workerEventLoopGroup(new NioEventLoopGroup(userDefinedProperties.getOrDefault("WORKER_EVENT_LOOP_THREADS", defaultProperties.get("WORKER_EVENT_LOOP_THREADS"))))
+                .bossEventLoopGroup(new NioEventLoopGroup(userDefinedProperties.getOrDefault("BOSS_EVENT_LOOP_THREADS", defaultProperties.get("BOSS_EVENT_LOOP_THREADS"))))
+                .channelType(NioServerSocketChannel.class)
+                .keepAliveTime(userDefinedProperties.getOrDefault("KEEP_ALIVE_TIME_SECS", defaultProperties.get("KEEP_ALIVE_TIME_SECS")), TimeUnit.SECONDS)
+                .keepAliveTimeout(userDefinedProperties.getOrDefault("KEEP_ALIVE_TIMEOUT_SECS", defaultProperties.get("KEEP_ALIVE_TIMEOUT_SECS")), TimeUnit.SECONDS)
+                .flowControlWindow(userDefinedProperties.getOrDefault("FLOW_CONTROL_WINDOW_BYTES", defaultProperties.get("FLOW_CONTROL_WINDOW_BYTES")))
                 .build()
                 .start();
 
-        logger.info("Started server with max conn age of " + maxConnTime + " seconds, " + numThreads + " threads, " +
-                "and " + maxConcCalls + " max concurrent calls per connection.");
+        logger.info("Started server with the following user-provided properties: " + userDefinedProperties);
+        logger.info("All other properties utilize the default values: " + defaultProperties);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.err.println("*** shutting down gRPC server since JVM is shutting down");
@@ -176,49 +196,27 @@ public class RouterServer {
      * Required arg:
      * GH config file
      *
-     * Optional args, specified with <arg name>=<arg value>:
-     * number of server threads (arg name: num_threads; default: 3)
-     * max connection time (arg name: max_conn_time; default: 10 seconds)
-     * max concurrent calls per connection (arg name: max_conc_calls; default: 500)
-     *
-     * Example:
-     * java -server -Xmx16g -Xms1g -XX:+UseG1GC -XX:MetaspaceSize=100M \
-     * -classpath grpc/target/graphhopper-grpc-1.0-SNAPSHOT.jar  RouterServer default_gh_config.yaml \
-     * max_conn_time=10 max_conn_calls=100
+     * Optional parameters, specified in RouterServer.SETTABLE_PARAMETERS, are passed in by being set as
+     * environment variables; for any parameters not provided by the user in this fashion, the defaults
+     * specified in the makeDefaultPropertiesMap() function below will be used instead.
      *
      */
     public static void main(String[] args) throws Exception {
-        if (!(args.length >= 1)) {
+        if (args.length != 1) {
             throw new RuntimeException("Must include path to GH config in order to start server!");
         }
         String config = args[0];
 
-        // Set defaults for other args
-        int numThreads = 2;
-        int maxConnTime = 100;
-        int maxConcCalls = 500;
-
-        // Parse any non-config args that were passed in
-        for (int i = 1; i < args.length; i++) {
-            String argName = args[i].split("=")[0];
-            String argValue = args[i].split("=")[1];
-            switch(argName) {
-                case "num_threads":
-                    numThreads = Integer.parseInt(argValue);
-                    break;
-                case "max_conc_calls":
-                    maxConcCalls = Integer.parseInt(argValue);
-                    break;
-                case "max_conn_time":
-                    maxConnTime = Integer.parseInt(argValue);
-                    break;
-                default:
-                    logger.warn(argName + " was passed in, but is not recognized as a valid argument! Check for typos?");
+        final Map<String, Integer> defaultProperties = makeDefaultPropertiesMap();
+        Map<String, Integer> userProvidedProperties = Maps.newHashMap();
+        Map<String, String> envVars = System.getenv();
+        for (String key: envVars.keySet()) {
+            if (RouterServer.SETTABLE_PARAMETERS.contains(key)) {
+                userProvidedProperties.put(key, Integer.parseInt(envVars.get(key)));
             }
-
         }
 
-        final RouterServer server = new RouterServer(config, numThreads, maxConnTime, maxConcCalls);
+        final RouterServer server = new RouterServer(config, defaultProperties, userProvidedProperties);
         server.start();
         server.blockUntilShutdown();
     }
@@ -235,5 +233,20 @@ public class RouterServer {
         public void run(MyConfiguration configuration, Environment environment) throws Exception {
             environment.servlets().addServlet("grpc-web", GrpcWebTrafficServlet.class).addMapping("/api/*");
         }
+    }
+
+    private static Map<String, Integer> makeDefaultPropertiesMap() {
+        Map<String, Integer> defaultProperties = Maps.newHashMap();
+        defaultProperties.put("SERVER_THREADS", 3);
+        defaultProperties.put("BOSS_EVENT_LOOP_THREADS", 1);
+        defaultProperties.put("WORKER_EVENT_LOOP_THREADS", 2);
+        defaultProperties.put("CONN_TIME_MAX_AGE_SECS", 120);
+        defaultProperties.put("CONN_TIME_GRACE_PERIOD_SECS", 60);
+        defaultProperties.put("MAX_CONC_CALLS_PER_CONN", 500);
+        // Defaults for below settings are all GRPC defaults
+        defaultProperties.put("KEEP_ALIVE_TIME_SECS", (int) (GrpcUtil.DEFAULT_SERVER_KEEPALIVE_TIME_NANOS * 1e-9));
+        defaultProperties.put("KEEP_ALIVE_TIMEOUT_SECS", (int) (GrpcUtil.DEFAULT_SERVER_KEEPALIVE_TIMEOUT_NANOS * 1e-9));
+        defaultProperties.put("FLOW_CONTROL_WINDOW_BYTES", NettyServerBuilder.DEFAULT_FLOW_CONTROL_WINDOW);
+        return defaultProperties;
     }
 }
--- web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
+++ web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
@@ -139,6 +139,7 @@ public class GraphHopperManaged implements Managed {
         graphHopper.setEncodedValueFactory(new EncodedValueFactoryWithStableId());
         graphHopper.init(configuration);
         graphHopper.setPathDetailsBuilderFactory(new PathDetailsBuilderFactoryWithStableId());
+        graphHopper.setAllowWrites(false);
     }
 
     @Override
