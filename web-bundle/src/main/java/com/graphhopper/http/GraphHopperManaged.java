b7b7e1fcd423023154ce569e0f57d149caf8d815
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-05-11 03:47:49 -0400

    Keep setAllowWrites but to true w/ added todo

--- web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
+++ web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
@@ -139,6 +139,7 @@ public class GraphHopperManaged implements Managed {
         graphHopper.setEncodedValueFactory(new EncodedValueFactoryWithStableId());
         graphHopper.init(configuration);
         graphHopper.setPathDetailsBuilderFactory(new PathDetailsBuilderFactoryWithStableId());
+        graphHopper.setAllowWrites(true); // TODO: Set via envvar
     }
 
     @Override
